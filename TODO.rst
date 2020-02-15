629eeaa4cdae87ba434967f63f80c35052be05d2
Alexis Bellido <alexis@ventanazul.com>
2020-02-14 21:24:36 -0500

    examples

---
+++ TODO.rst
@@ -0,0 +1,16 @@
+TODO
+===========================
+
+Using sets.
+
+Search in a list.
+
+Remove duplicates in the fastest way (chapter 18 of cookbook)
+
+Fibonacci sequence.
+
+Using map.
+
+Use timeit with chapter 18 of cookbook.
+
+Algorithms book.
--- running-time.py
+++ running-time.py
@@ -1,9 +1,20 @@
-from time import time
+from time import time, process_time
 
+"""
+Running with timeit, which is better than the time module.
+
+python -m timeit '"-".join(str(n) for n in range(100))'
+
+python  -m timeit '"-".join([str(n) for n in range(100)])'
+
+python -m timeit '"-".join(map(str, range(100)))'
+"""
 
 def measure_by_wall_clock(iterations):
     """
-    The time function measures relative to wall clock.
+    The time.time function measures relative to wall clock so it's accuracy will
+    depend on what other processes are running on the computer.
+    It returns a value in seconds.
     """
 
     start_time = time()
@@ -12,12 +23,40 @@ def measure_by_wall_clock(iterations):
         pass
     end_time = time()
     elapsed = end_time - start_time
-    print(f'Elapsed time: {elapsed}')
+    print(f"Elapsed time.time: {elapsed}\n")
 
+def measure_by_process_time(iterations):
+    """
+    Return the value (in fractional seconds) of the sum of the system and
+    user CPU time of the current process.
+    """
+
+    start_time = process_time()
+    print(f"Running {iterations} iterations...")
+    for i in range(iterations):
+        pass
+    end_time = process_time()
+    elapsed = end_time - start_time
+    print(f"Elapsed time.process_time: {elapsed}\n")
+
+def measure_by_timeit(iterations):
+    """
+    Return the value (in fractional seconds) of the sum of the system and
+    user CPU time of the current process.
+    """
+
+    start_time = process_time()
+    print(f"Running {iterations} iterations...")
+    for i in range(iterations):
+        pass
+    end_time = process_time()
+    elapsed = end_time - start_time
+    print(f"Elapsed time.process_time: {elapsed}\n")
 
 if __name__ == '__main__':
     try:
         iterations = int(input("How many iterations?\n"))
     except ValueError:
-        iterations = 20000000
+        iterations = 2000000
     measure_by_wall_clock(iterations)
+    measure_by_process_time(iterations)
