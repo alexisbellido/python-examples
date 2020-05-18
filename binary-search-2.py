792dc2befbeb40854313df825cd354cd49415b24
Alexis Bellido <alexis@ventanazul.com>
2020-05-18 16:36:15 -0400

    search

--- binary-search-2.py
+++ binary-search-2.py
@@ -8,8 +8,9 @@ def binary_search(alist, item):
     last = len(alist) - 1
     found = False
 
-    while first <= last and not found:
+    while first < last and not found:
         midpoint = (first + last) // 2
+        print('first', first, 'last', last)
         if alist[midpoint] == item:
             found = True
         else:
@@ -21,6 +22,9 @@ def binary_search(alist, item):
     return found
 
 if __name__ == '__main__':
-    target = int(input("Enter a target number\n"))
+    # target = int(input("Enter a target number\n"))
+    target = 21
     data = [1,4,5,6,8,10,13,20,21,24,27,29,33,36,41,43,50,61]
+    # sorted returns a new, sorted list
+    data = sorted(data)
     print(binary_search(data, target))
