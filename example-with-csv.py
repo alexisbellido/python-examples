33f291f78ac80275a7142adaa8aef7b5fb4515aa
Alexis Bellido <a@zinibu.com>
2022-09-27 11:16:18 -0400

    inputs

--- example-with-csv.py
+++ example-with-csv.py
@@ -1,4 +1,6 @@
+from pprint import pprint
 from datetime import datetime, date, time, timedelta
+from csv import DictReader
 
 def get_input_file(path):
     lines = []
@@ -8,6 +10,14 @@ def get_input_file(path):
             lines.append(line)
     return lines
 
+
+def get_csv_file(path, fieldnames=None):
+    rows = []
+    with open(path, "r", encoding="utf-8") as csv:
+        reader = DictReader(csv, fieldnames)
+        for row in reader:
+            rows.append(row)
+    return rows
 
 # >>> c1 = deque(['p3', 'p2', 'p1', 'p4', 'p1', 'p2', 'p5'])
 # >>> c1
@@ -60,12 +70,31 @@ def build_groups(pages):
 
 if __name__ == '__main__':
 
-    path = 'input1.txt'
+    path_txt = 'input1.txt'
     # path = 'input2.txt'
-    lines = get_input_file(path)
+    lines = get_input_file(path_txt)
 
-    page_paths = build_page_paths(lines)
-    print(page_paths)
+    # print(lines)
+    pprint(lines)
+
+    print("\n=================\n")
+    
+    # use first row as field names
+    path_csv = 'input1.csv'
+    rows = get_csv_file(path_csv)
+    pprint(rows)
+    # print(rows)
+
+    print("\n=================\n")
+
+    # pass field names
+    path_csv = 'input1.txt'
+    rows = get_csv_file(path_csv, fieldnames=['id', 'start', 'end', 'visits'])
+    pprint(rows)
+    # print(rows)
+
+    # page_paths = build_page_paths(lines)
+    # print(page_paths)
     # for user, pages in page_paths.items():
     #     print(user)
     #     print(pages)
